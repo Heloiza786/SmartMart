7ac909fdd04b72299b7904cbe59bc2e420498b36
Heloiza786 <heloizacustodio786@gmail.com>
2026-01-10 11:36:33 -0300

    feat: add readme and cors

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database.database import engine, Base
 from app.database.models import CategoryModel, ProductModel, SaleModel
-from app.route import sales
-from app.route import category
-from app.route import products
+from app.route import sales, category, products
 
 app = FastAPI(title="SmartMart API")
 
 
-Base.metadata.create_all(bind=engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+Base.metadata.create_all(bind=engine)
 
 app.include_router(sales.router)
 app.include_router(category.router)
